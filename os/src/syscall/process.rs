174111c43252dd3d81cde0e9db0eb3d514fa0fb5
xbo1 <wangshyn@gmail.com>
2025-04-17 17:52:11 +0800

    完成ch5

--- os/src/syscall/process.rs
+++ os/src/syscall/process.rs
@@ -9,7 +9,8 @@ use crate::{
     task::{
         add_task, current_task, current_user_token, exit_current_and_run_next,
         suspend_current_and_run_next, syscall_map, syscall_unmap,
-    }, timer::get_time_us
+    },
+    timer::get_time_us,
 };
 
 #[repr(C)]
@@ -68,7 +69,11 @@ pub fn sys_exec(path: *const u8) -> isize {
 /// If there is not a child process whose pid is same as given, return -1.
 /// Else if there is a child process but it is still running, return -2.
 pub fn sys_waitpid(pid: isize, exit_code_ptr: *mut i32) -> isize {
-    //trace!("kernel: sys_waitpid");
+    trace!(
+        "kernel::pid[{}] sys_waitpid [{}]",
+        current_task().unwrap().pid.0,
+        pid
+    );
     let task = current_task().unwrap();
     // find a child process
 
@@ -130,7 +135,7 @@ pub fn sys_mmap(start: usize, len: usize, port: usize) -> isize {
     if port & !0x7 != 0 || port & 0x7 == 0 {
         return -1;
     }
-    if start % PAGE_SIZE != 0  {
+    if start % PAGE_SIZE != 0 {
         return -1;
     }
     syscall_map(start, len, port)
@@ -142,7 +147,7 @@ pub fn sys_munmap(start: usize, len: usize) -> isize {
         "kernel:pid[{}] sys_munmap NOT IMPLEMENTED",
         current_task().unwrap().pid.0
     );
-    if start % PAGE_SIZE != 0  {
+    if start % PAGE_SIZE != 0 {
         return -1;
     }
     syscall_unmap(start, len)
@@ -160,19 +165,38 @@ pub fn sys_sbrk(size: i32) -> isize {
 
 /// YOUR JOB: Implement spawn.
 /// HINT: fork + exec =/= spawn
-pub fn sys_spawn(_path: *const u8) -> isize {
-    trace!(
-        "kernel:pid[{}] sys_spawn NOT IMPLEMENTED",
-        current_task().unwrap().pid.0
-    );
+pub fn sys_spawn(path: *const u8) -> isize {
+    trace!("kernel:pid[{}] sys_spawn", current_task().unwrap().pid.0);
+    let path = translated_str(current_user_token(), path);
+    if let Some(app_inode) = open_file(path.as_str(), OpenFlags::RDONLY) {
+        let task = current_task().unwrap();
+        let new_task = task.fork();
+        let new_pid = new_task.pid.0;
+        // modify trap context of new_task, because it returns immediately after switching
+        let trap_cx = new_task.inner_exclusive_access().get_trap_cx();
+        // we do not have to move to next instruction since we have done it
+        trap_cx.x[10] = 0;
+        // add new task to scheduler
+        add_task(new_task.clone());
+        let all_data = app_inode.read_all();
+        new_task.exec(all_data.as_slice());
+        return new_pid as isize;
+    }
     -1
 }
 
 // YOUR JOB: Set task priority.
-pub fn sys_set_priority(_prio: isize) -> isize {
+pub fn sys_set_priority(prio: isize) -> isize {
     trace!(
-        "kernel:pid[{}] sys_set_priority NOT IMPLEMENTED",
+        "kernel:pid[{}] sys_set_priority",
         current_task().unwrap().pid.0
     );
+    if prio < 2 {
+        return -1;
+    }
+    if let Some(task) = current_task() {
+        task.inner_exclusive_access().priority = prio as usize;
+        return prio;
+    }
     -1
 }
